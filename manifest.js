e3d82dd7888f296329fdc2e9390958d78e55b3a2
Denis Buryachkovskiy <denis.buryachkovskiy@dataart.com>
2019-01-14 11:16:39 +0200

    Added doc for loops lesson

--- manifest.js
+++ manifest.js
@@ -5,6 +5,7 @@ module.exports = {
     short_name: 'Classes',
     description: 'Application for students of Hillel Classes',
     background_color: '#ffffff',
+
     icons: [{
         src: path.resolve(__dirname, './src/images/octopus.png'),
         sizes: [96, 128, 192, 256, 384, 512] // multiple sizes
